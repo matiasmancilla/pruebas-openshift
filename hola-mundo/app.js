d4785f3081a99754f18442472529ae86d9f44a60
Matias Mancilla <matiassnm.1@gmail.com>
2020-11-30 23:39:40 -0300

    Update message

--- hola-mundo/app.js
+++ hola-mundo/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hola Mundo - V1!\n');
+  res.send('Hola Mundo - Versión 1\n');
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('app listening on port 8080!!');
 });
 
